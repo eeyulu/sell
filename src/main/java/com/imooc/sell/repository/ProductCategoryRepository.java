c7754fe18ffc3ab5553b373d020a2919a75286d8
eeyulu <kqoo@163.com>
2018-02-05 18:46:08 +0800

    no message

--- src/main/java/com/imooc/sell/repository/ProductCategoryRepository.java
+++ src/main/java/com/imooc/sell/repository/ProductCategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
-    List<ProductCategory> findByCategoryTypeIn(List<Integer> CategoryTypeList);
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 }
