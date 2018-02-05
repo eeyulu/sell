99f5b4a67132366c79e3309ba2a8dd45b52e002d
eeyulu <kqoo@163.com>
2018-02-05 17:59:52 +0800

    测试jpa

---
+++ src/test/java/com/imooc/sell/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataObject.ProductCategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest(){
+        ProductCategory category = repository.findOne(1);
+        System.out.println(category.toString());
+    }
+}
